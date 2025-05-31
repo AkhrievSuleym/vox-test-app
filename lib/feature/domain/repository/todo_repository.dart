5a07ab72d6a901dfd27e3039e294b3155671ec5b
AkhrievSuleym <murid2005@mail.ru>
2025-06-01 02:28:42 +0300

    create pages

--- lib/feature/domain/repository/todo_repository.dart
+++ lib/feature/domain/repository/todo_repository.dart
@@ -1,8 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:vox_app/core/error/failures.dart';
 import 'package:vox_app/feature/domain/entities/category_entity.dart';
-
-import '../entities/todo_entity.dart';
+import 'package:vox_app/feature/domain/entities/todo_entity.dart';
 
 abstract class TodoRepository {
   Future<Either<Failure, List<TodoEntity>>> getTodos();
